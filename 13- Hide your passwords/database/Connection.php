8d66d1102442783e1c235399349ee39573f04572
Cojocaru Ionut Liviu <cojocaruionutliviu@gmail.com>
2021-03-14 20:31:43 +0200

    Hide your pass

---
+++ 13- Hide your passwords/database/Connection.php
@@ -0,0 +1,23 @@
+<?php
+
+class Connection{
+
+  public function make($config)
+
+  {
+
+    try {
+      return new PDO(
+        $config['connection']. ';dbname='.$config['name'],
+        $config['username'],
+        $config['password'],
+      );
+    } catch (PDOException $e){
+      die($e->getMessage());
+    } 
+
+  }
+
+}
+
+// $pdo = Connection::make();
